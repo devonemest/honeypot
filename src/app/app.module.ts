9b626c4138880cac5cee0bc89d5e580e2f98d482
GoldGem <mishachemo@gmail.com>
2020-04-28 12:39:10 +0300

    add firebase
    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
 import { GeoComponent } from './geo/geo.component';
 import {WebSpeechModule} from "./web-speech/web-speech.module";
-import {FormsModule} from "@angular/forms";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from "./shared/shared.module";
 import { FingerprintComponent } from './fingerprint/fingerprint.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { environment } from '../environments/environment';
+import { AsyncPipe } from '@angular/common';
+import {MessagingService} from "./service/messaging.service";
 
 @NgModule({
   declarations: [
@@ -16,13 +24,19 @@ import { FingerprintComponent } from './fingerprint/fingerprint.component';
     FingerprintComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     WebSpeechModule,
     FormsModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+
   ],
-  providers: [],
+  providers: [MessagingService,AsyncPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
